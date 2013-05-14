2caa10d97e2e68abf3283d9bc46d2052e75e9e15
Sam Bao <sambao21@gmail.com>
2013-05-13 21:10:43 -0500

    create oh-my-zshell custom plugin directory

--- sambao21_workstation/recipes/zsh_syntax_highlighting.rb
+++ sambao21_workstation/recipes/zsh_syntax_highlighting.rb
@@ -1,3 +1,10 @@
+directory "#{WS_HOME}/.oh-my-zsh/custom/plugins/" do
+  action :create
+  owner WS_USER
+  mode "0700"
+  only_if { !::File.exists?("#{WS_HOME}/.oh-my-zsh/custom/plugins") }
+end
+
 execute "install zsh-syntax-highlighting" do
   command "git clone git://github.com/zsh-users/zsh-syntax-highlighting.git"
   user WS_USER
