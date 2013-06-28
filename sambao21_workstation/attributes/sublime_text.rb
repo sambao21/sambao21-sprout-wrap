78a04ae71174c76321869dfbdc98b7576814daad
Sam Bao <sambao21@gmail.com>
2013-06-28 08:09:14 -0500

    remove livecss sublime plug in because its 404

--- sambao21_workstation/attributes/sublime_text.rb
+++ sambao21_workstation/attributes/sublime_text.rb
@@ -1,12 +1,9 @@
-# coding: UTF-8
-
 # Sort alphabetically by name
 node.override["sublime_text_packages"] = [
   {"name" => "Auto Encoding for Ruby", "source" => "https://github.com/elomarns/auto-encoding-for-ruby"},
   {"name" => "DetectSyntax", "source" => "https://github.com/phillipkoebbe/DetectSyntax"},
   {"name" => "GitGutter", "source" => "https://github.com/jisaacks/GitGutter"},
   {"name" => "LESS", "source" => "https://github.com/danro/LESS-sublime"},
-  {"name" => "Live CSS", "source" => "https://github.com/a-sk/livecss"},
   {"name" => "Markdown Preview", "source" => "https://github.com/revolunet/sublimetext-markdown-preview"},
   {"name" => "SublimeCodeIntel", "source" => "http://github.com/Kronuz/SublimeCodeIntel"},
   {"name" => "LiveReload", "source" => "https://github.com/dz0ny/LiveReload-sublimetext2"},
