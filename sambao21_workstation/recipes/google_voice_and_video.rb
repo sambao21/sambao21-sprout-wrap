810a473e69d647198afcf709ec106f17236b3ccf
Sam Bao <sambao21@gmail.com>
2013-06-29 22:21:13 -0400

    Update google voice and video

--- sambao21_workstation/recipes/google_voice_and_video.rb
+++ sambao21_workstation/recipes/google_voice_and_video.rb
@@ -1,7 +1,6 @@
 dmg_package "Google Voice And Video" do
-  volumes_dir "GoogleVoiceAndVideoAccelSetup_3.16.0.12200"
+  volumes_dir "GoogleVoiceAndVideoAccelSetup_4.0.3.13724"
   source "https://dl.google.com/googletalk/googletalkplugin/GoogleVoiceAndVideoSetup.dmg"
-  checksum "428830e27e2b9e34dd570574f6ee03fd1194eb9d45e20b1945a1c3c72498092e"
   owner node['current_user']
   type "pkg"
   package_id "com.google.pkg.GoogleVoiceAndVideo"
