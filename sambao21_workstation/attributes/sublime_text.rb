816d128f50a1b48ff525b49c32fdf033dbfd0f4a
Sam Bao <sambao21@gmail.com>
2013-07-11 23:08:02 -0500

    sublime install html2haml and sass syntax highlighting

--- sambao21_workstation/attributes/sublime_text.rb
+++ sambao21_workstation/attributes/sublime_text.rb
@@ -9,5 +9,7 @@ node.override["sublime_text_packages"] = [
   {"name" => "LiveReload", "source" => "https://github.com/dz0ny/LiveReload-sublimetext2"},
   {"name" => "JsFormat", "source" => "https://github.com/jdc0589/JsFormat"},
   {"name" => "SublimeLinter", "source" => "https://github.com/SublimeLinter/SublimeLinter"},
-  {"name" => "CTags", "source" => "https://github.com/SublimeText/CTags"}
+  {"name" => "CTags", "source" => "https://github.com/SublimeText/CTags"},
+  {"name" => "Syntax highlighting for Sass", "source" => "https://github.com/P233/Syntax-highlighting-for-Sass"},
+  {"name" => "HTML2HAML", "source" => "https://github.com/pavelpachkovskij/sublime-html-to-haml"}
 ]
