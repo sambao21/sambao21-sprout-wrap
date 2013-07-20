71e72d6a4bd203f8f6ce0effc108685c45fb3298
Sam Bao <sambao21@gmail.com>
2013-07-20 18:51:11 -0500

    fix extra parens in libraries/brew_cask.rb

--- sambao21_workstation/libraries/brew_cask.rb
+++ sambao21_workstation/libraries/brew_cask.rb
@@ -1,7 +1,7 @@
 class Chef::Recipe
   def brew_cask_installed?(package)
     include_recipe "sprout-osx-base::homebrew"
-    installed=(system("brew cask info #{package} > /dev/null 2>&1")
+    installed = system("brew cask info #{package} > /dev/null 2>&1")
     Chef::Log.debug("brew cask package #{package} " + (installed ? "IS" : "IS NOT") + " installed.")
     installed
   end
