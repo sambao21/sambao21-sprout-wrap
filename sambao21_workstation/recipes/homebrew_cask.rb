194caedf836697cb6d9ad44e504ad66fae298ef3
Sam Bao <sambao21@gmail.com>
2013-07-20 18:46:11 -0500

    include homebrew recipe before installing brew cask

--- sambao21_workstation/recipes/homebrew_cask.rb
+++ sambao21_workstation/recipes/homebrew_cask.rb
@@ -1,3 +1,5 @@
+include_recipe "sprout-osx-base::homebrew"
+
 execute "tap phinze/homebrew-cask" do
   command "brew tap phinze/homebrew-cask"
   not_if { system("brew tap | grep 'cask' > /dev/null 2>&1") }
