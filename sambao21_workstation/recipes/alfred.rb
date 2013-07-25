bd94190a51a5bd2266773e15a71328349d48b8f4
Sam Bao <sambao21@gmail.com>
2013-07-24 20:15:05 -0500

    when install alfred, link it with cask

--- sambao21_workstation/recipes/alfred.rb
+++ sambao21_workstation/recipes/alfred.rb
@@ -1,2 +1,8 @@
 include_recipe "homebrew::cask"
 homebrew_cask "alfred"
+
+execute 'brew cask alfred link' do
+  command 'brew cask alfred link'
+  user node['current_user']
+  not_if { system("brew cask alfred status | grep 'happily linked' > /dev/null 2>&1") }
+end
