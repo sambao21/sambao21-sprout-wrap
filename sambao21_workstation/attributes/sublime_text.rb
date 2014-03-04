cadd87230bfbf19fe5e3de88409250af0a78f4a2
Sam Bao <sambao21@gmail.com>
2014-03-03 22:13:39 -0600

    add better coffeescript to sublime plugins

--- sambao21_workstation/attributes/sublime_text.rb
+++ sambao21_workstation/attributes/sublime_text.rb
@@ -1,17 +1,19 @@
 # Sort alphabetically by name
 node.override["sublime_text_packages"] = [
-  {"name" => "Auto Encoding for Ruby", "source" => "https://github.com/elomarns/auto-encoding-for-ruby"},
-  {"name" => "GitGutter", "source" => "https://github.com/jisaacks/GitGutter"},
-  {"name" => "LESS", "source" => "https://github.com/danro/LESS-sublime"},
-  {"name" => "Markdown Preview", "source" => "https://github.com/revolunet/sublimetext-markdown-preview"},
-  {"name" => "SublimeCodeIntel", "source" => "http://github.com/Kronuz/SublimeCodeIntel"},
-  {"name" => "LiveReload", "source" => "https://github.com/dz0ny/LiveReload-sublimetext2"},
-  {"name" => "JsFormat", "source" => "https://github.com/jdc0589/JsFormat"},
-  {"name" => "SublimeLinter", "source" => "https://github.com/SublimeLinter/SublimeLinter"},
+  {"name" => "ApplySyntax", "source" => "https://github.com/facelessuser/ApplySyntax.git"},
+  {"name" => "Better CoffeeScript", "source" => "https://github.com/aponxi/sublime-better-coffeescript.git"},
   {"name" => "CTags", "source" => "https://github.com/SublimeText/CTags"},
-  {"name" => "Syntax highlighting for Sass", "source" => "https://github.com/P233/Syntax-highlighting-for-Sass"},
+  {"name" => "GitGutter", "source" => "https://github.com/jisaacks/GitGutter"},
   {"name" => "HTML2HAML", "source" => "https://github.com/pavelpachkovskij/sublime-html-to-haml"},
+  {"name" => "JsFormat", "source" => "https://github.com/jdc0589/JsFormat"},
+  {"name" => "LESS", "source" => "https://github.com/danro/LESS-sublime"},
+  {"name" => "LiveReload", "source" => "https://github.com/dz0ny/LiveReload-sublimetext2"},
+  {"name" => "Markdown Preview", "source" => "https://github.com/revolunet/sublimetext-markdown-preview"},
+  {"name" => "Ruby Coverage", "source" => "https://github.com/integrum/SublimeRubyCoverage.git"},
+  {"name" => "Ruby Tests", "source" => "https://github.com/maltize/sublime-text-2-ruby-tests"},
   {"name" => "Sublime Alignment", "source" => "https://github.com/wbond/sublime_alignment"},
+  {"name" => "SublimeCodeIntel", "source" => "http://github.com/Kronuz/SublimeCodeIntel"},
   {"name" => "SublimeERB", "source" => "https://github.com/eddorre/SublimeERB"},
-  {"name" => "Sublime Text 2 Ruby Tests", "source" => "https://github.com/maltize/sublime-text-2-ruby-tests"}
+  {"name" => "SublimeLinter", "source" => "https://github.com/SublimeLinter/SublimeLinter"},
+  {"name" => "Syntax highlighting for Sass", "source" => "https://github.com/P233/Syntax-highlighting-for-Sass"}
 ]
